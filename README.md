c38739ce259ad2ecd3ce6aa03113daa8408a35e8
Heikki Alanen <heikki.alanen@metropolia.fi>
2014-10-05 22:40:24 +0300

    Basic functionality added.

---
+++ README.md
@@ -0,0 +1,4 @@
+hal-ksgpx-cleaner
+==============
+
+Removes comma, fullstop and colon characters from gpx-file name-field. This is because Windows Phone version of Karttaselain does not support them when importing waypoints.
---
+++ hal-ksgpx-cleaner.js
@@ -0,0 +1,35 @@
+﻿var fs = require('fs')
+
+var LineByLineReader = require('line-by-line');
+var lr = new LineByLineReader('./passit/passit_20131118-wo-track.gpx');
+var output = "";
+var i = 1;
+OPENTAG = "<name>";
+CLOSETAG = "</name>";
+
+lr.on('error', function (err) {
+    // 'err' contains error object
+});
+
+lr.on('line', function (line) {
+	var nameOpenTag = line.search(OPENTAG) + OPENTAG.length;
+	var nameCloseTag = line.search(CLOSETAG);
+    if (nameOpenTag > OPENTAG.length) {
+		var nameOrig = line.slice(nameOpenTag, nameCloseTag);
+		var tmp = nameOrig.replace(/[,:.]/gi, "");
+		var newLine = line.replace(nameOrig, tmp);
+		//output += i + " löytyi " + line + " -> " + newLine + "\r\n";
+		output += newLine  + "\r\n";
+		i++;
+	} else {
+		output += line + "\r\n";
+	}
+});
+
+lr.on('end', function () {
+    // All lines are read, file is closed now.
+	fs.writeFile('./passit/passit_20131118-wo-track-mod.gpx', output, function (err) {
+		if (err) throw err;
+		console.log('It\'s saved!');
+	});
+});
