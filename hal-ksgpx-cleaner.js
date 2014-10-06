aa13976bf20faf0c8b533eea738261974c3c5a16
Heikki Alanen <heikki.alanen@metropolia.fi>
2014-10-06 19:31:14 +0300

    Tracks removed from file.

--- hal-ksgpx-cleaner.js
+++ hal-ksgpx-cleaner.js
@@ -1,34 +1,56 @@
 ﻿var fs = require('fs')
 
 var LineByLineReader = require('line-by-line');
-var lr = new LineByLineReader('./passit/passit_20131118-wo-track.gpx');
+var lr = new LineByLineReader('./passit/passit_20131118.gpx');
 var output = "";
 var i = 1;
-OPENTAG = "<name>";
-CLOSETAG = "</name>";
+var trkOpenTagFound = false;
+
+NAMEOPENTAG = "<name>";
+NAMECLOSETAG = "</name>";
+TRACKOPENTAG = "<trk>";
+TRACKCLOSETAG = "</trk>";
 
 lr.on('error', function (err) {
     // 'err' contains error object
 });
 
 lr.on('line', function (line) {
-	var nameOpenTag = line.search(OPENTAG) + OPENTAG.length;
-	var nameCloseTag = line.search(CLOSETAG);
-    if (nameOpenTag > OPENTAG.length) {
-		var nameOrig = line.slice(nameOpenTag, nameCloseTag);
-		var tmp = nameOrig.replace(/[,:.]/gi, "");
-		var newLine = line.replace(nameOrig, tmp);
-		//output += i + " löytyi " + line + " -> " + newLine + "\r\n";
-		output += newLine  + "\r\n";
-		i++;
-	} else {
-		output += line + "\r\n";
+	var nameOpenTag = line.search(NAMEOPENTAG);
+	var nameCloseTag = line.search(NAMECLOSETAG);
+	var trackOpenTag = line.search(TRACKOPENTAG);
+	var trackCloseTag = line.search(TRACKCLOSETAG);
+	
+	//console.log(nameOpenTag + " " + nameCloseTag + " " + trackOpenTag + "\r\n");
+	
+	if (trackOpenTag >= 0) {
+		trkOpenTagFound = true;
+	}
+
+	// If track is not found yet, check the line content
+	// If track is found then skip the line
+	if (trkOpenTagFound === false) {
+		if (nameOpenTag >= 0) {
+			var nameOrig = line.slice(nameOpenTag + NAMEOPENTAG.length, nameCloseTag);
+			var tmp = nameOrig.replace(/[,:.]/gi, "");
+			var newLine = line.replace(nameOrig, tmp);
+			//output += i + " löytyi " + line + " -> " + newLine + "\r\n";
+			//console.log(newLine + "\r\n");
+			output += newLine  + "\r\n";
+			i++;
+		} else {
+			output += line + "\r\n";
+		}
+	}
+	// Track closed
+	if ((trackCloseTag >= 0) && (trkOpenTagFound === true)) {
+		trkOpenTagFound = false;
 	}
 });
 
 lr.on('end', function () {
     // All lines are read, file is closed now.
-	fs.writeFile('./passit/passit_20131118-wo-track-mod.gpx', output, function (err) {
+	fs.writeFile('./passit/passit_20131118-mod.gpx', output, function (err) {
 		if (err) throw err;
 		console.log('It\'s saved!');
 	});
