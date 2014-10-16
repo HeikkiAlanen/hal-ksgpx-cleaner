f235b2d736667a6dfdc842ebc2efbca34782ee24
Heikki Alanen <heikki.alanen@metropolia.fi>
2014-10-16 15:06:57 +0300

    Remove <extension> fields.

--- hal-ksgpx-cleaner.js
+++ hal-ksgpx-cleaner.js
@@ -3,12 +3,14 @@ var fs = require('fs');
 
 var output = "";
 var i = 1;
-var trkOpenTagFound = false;
+var skipLine = false;
 
 NAMEOPENTAG = "<name>";
 NAMECLOSETAG = "</name>";
 TRACKOPENTAG = "<trk>";
 TRACKCLOSETAG = "</trk>";
+EXTOPENTAG = "<extensions>";
+EXTCLOSETAG = "</extensions>";
 
 // read all lines:
 lineReader.eachLine('./passit/passit_2014.gpx', function(line) {
@@ -16,14 +18,16 @@ lineReader.eachLine('./passit/passit_2014.gpx', function(line) {
 	var nameCloseTag = line.search(NAMECLOSETAG);
 	var trackOpenTag = line.search(TRACKOPENTAG);
 	var trackCloseTag = line.search(TRACKCLOSETAG);
+	var extOpenTag = line.search(EXTOPENTAG);
+	var extCloseTag = line.search(EXTCLOSETAG);
 	
-	if (trackOpenTag >= 0) {
-		trkOpenTagFound = true;
+	if ((trackOpenTag >= 0) || (extOpenTag >= 0)) {
+		skipLine = true;
 	}
 
 	// If track is not found yet, check the line content
 	// If track is found then skip the line
-	if (trkOpenTagFound === false) {
+	if (skipLine === false) {
 		if (nameOpenTag >= 0) {
 			var nameOrig = line.slice(nameOpenTag + NAMEOPENTAG.length, nameCloseTag);
 			var tmp = nameOrig.replace(/[,:.]/gi, "");
@@ -35,8 +39,8 @@ lineReader.eachLine('./passit/passit_2014.gpx', function(line) {
 		}
 	}
 	// Track closed
-	if ((trackCloseTag >= 0) && (trkOpenTagFound === true)) {
-		trkOpenTagFound = false;
+	if (((trackCloseTag >= 0) || (extCloseTag >= 0)) && (skipLine === true)) {
+		skipLine = false;
 	}
 }).then(function () {
 	console.log("I'm done!!");
